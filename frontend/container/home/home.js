5a32a989a180099cff7cb676247ffc1cba3b4393
Mathias Hobler <hoblermathias@icloud.com>
2022-05-11 14:52:27 +0200

    add javascript fkt

---
+++ frontend/container/home/home.js
@@ -0,0 +1,12 @@
+const showHideButton = document.querySelector('[data-js="button-showHide"]');
+const answer = document.querySelector(".question-card__answer");
+
+showHideButton.addEventListener("click", () => {
+  if (showHideButton.innerHTML === "Show Answer") {
+    showHideButton.innerHTML = "Hide Answer";
+    answer.classList.remove("hide");
+  } else {
+    showHideButton.innerHTML = "Show Answer";
+    answer.classList.add("hide");
+  }
+});
